a3b07c14a366ed114d4c34f493d6445f9aa8b084
matyhattam <maty.hattam@gmail.com>
2024-11-10 22:00:44 +0700

    react-router-dom + routes for error and signin/up

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
+import ErrorPage from './routes/error-page.tsx';
+import LoginPage from './routes/login-page.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
