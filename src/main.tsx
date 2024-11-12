122a505405a89fa809b0f14f816fdc08b9b7ece1
matyhattam <maty.hattam@gmail.com>
2024-11-12 15:56:07 +0700

    added routing depending if user is connected or not

--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,27 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
-import App from './App.tsx'
 import ErrorPage from './routes/error-page.tsx';
 import LoginPage from './routes/login-page.tsx';
+import Wrapper from './wrapper.tsx';
+import App from './App.tsx';
 
 const router = createBrowserRouter([
+
   {
     path: "/",
-    element: <App />,
+    element: <Wrapper />,
     errorElement: <ErrorPage />
   },
   {
     path: "/login",
     element: <LoginPage />,
   },
+  {
+    path: "/app",
+    element: <App />,
+  },
+
 ]);
 
 createRoot(document.getElementById('root')!).render(
